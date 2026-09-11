f9563c7a1aa3e25639c16c0a218a1a9652ab9a98
Adam Kurbiel <ad.kurbiel@gmail.com>
2026-09-11 19:32:16 +0200

    added smoothness animation

---
+++ app.js
@@ -0,0 +1,23 @@
+const text = document.querySelector(".fixed");
+
+const maxSize = 18;
+const minSize = 5;
+const scrollDistance = 600;
+const smoothness = 0.5
+
+let currentSize = maxSize;
+let targetSize = maxSize;
+
+function update() {
+    const progress = Math.min(window.scrollY / scrollDistance, 1);
+
+    targetSize = maxSize - (maxSize - minSize) * progress;
+
+    currentSize += (targetSize - currentSize) * smoothness;
+
+    text.style.fontSize = `${currentSize}vw`;
+
+    requestAnimationFrame(update);
+}
+
+update();
